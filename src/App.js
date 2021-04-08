eb6b7d5accce1fdbb26dd9a0d7a132b3783b27db
yonattan95 <yonattan95@gmail.com>
2021-04-08 16:01:09 -0500

    editor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h4 className="App">Titulo del editor</h4>
+      <CKEditor
+        editor={ClassicEditor}
+        data=""
+        onReady={editor => {
+          // You can store the "editor" and use when it is needed.
+          console.log('Editor is ready to use!', editor);
+        }}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          setData(data);
+          console.log({ event, editor, data });
+        }}
+        onBlur={(event, editor) => {
+          console.log('Blur.', editor);
+        }}
+        onFocus={(event, editor) => {
+          console.log('Focus.', editor);
+        }}
+      />
+      <div>
+        <h4 className="App">Visualidor de data</h4>
+        <div className="view-data">
+          <div>{data}</div>
+          <div className="divider" />
+          <div dangerouslySetInnerHTML={{ __html: data }} />
+        </div>
+      </div>
     </div>
   );
 }
